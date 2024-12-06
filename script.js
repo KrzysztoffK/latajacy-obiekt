82009c4212005a82a006fcbeeb54ce01dad16f91
Krzysztof <krzysztof.kolasinski@tm1.edu.pl>
2024-12-06 14:11:01 +0100

    Added collisions.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//Collection of user collectable static nodes
+let staticObjectNodes = document.querySelectorAll(".present");
+
+//User movable object
 const userObject = document.querySelector("#userObject");
 const userObjectRect = userObject.getBoundingClientRect();
 const userObjectWidth = userObjectRect.width;
@@ -6,11 +10,12 @@ const userObjectHeight = userObjectRect.height;
 let posX = 100;
 let posY = 100;
 
-
+//Define and calculate linear and diagonal move speeds
 const linearMoveSpeed = 10;
 const diagonalMoveSpeed = linearMoveSpeed / Math.sqrt(2);
 let moveSpeed = linearMoveSpeed;
 
+//Register and hold state of pressed keys to determine which way to move the userObject
 const keysPressed = {
     ArrowUp: false,
     ArrowDown: false,
@@ -18,15 +23,26 @@ const keysPressed = {
     ArrowRight: false
 };
 
+//Detect collision between objects by using getBoundingClientRect() method 
+//to get the object boundaries and check if they overlap
+function detectCollision(rect1, rect2){
+    return (
+        rect1.left < rect2.right && 
+        rect2.left < rect1.right &&
+        rect1.top < rect2.bottom &&
+        rect1.bottom > rect2.top
+    );
+}
+
 function updatePosition(){
     if (keysPressed.ArrowUp) {
-        if(!((posY - moveSpeed)<0)){posY -= moveSpeed;}
+        if(!((posY - moveSpeed)<0)){posY -= moveSpeed;};
     }
     if (keysPressed.ArrowDown) {
         if(!(posY + userObjectHeight + moveSpeed > document.documentElement.clientHeight)){posY += moveSpeed};
     }
     if (keysPressed.ArrowLeft) {
-        if(!((posX - moveSpeed)<0)){posX -= moveSpeed;} 
+        if(!((posX - moveSpeed)<0)){posX -= moveSpeed;} ;
     }
     if (keysPressed.ArrowRight) {
         if(!(posX + userObjectWidth + moveSpeed > document.documentElement.clientWidth)){posX += moveSpeed};
@@ -34,6 +50,20 @@ function updatePosition(){
     
     userObject.style.top = posY + 'px';
     userObject.style.left = posX + 'px';
+
+    //After every movement update the user movable object rectangle
+    const userRect = userObject.getBoundingClientRect();
+
+    //Loop through a list of all the user collectible nodes and check for a collision
+    staticObjectNodes.forEach((staticObject, index) => {
+        const staticRect = staticObject.getBoundingClientRect();
+        if(detectCollision(userRect, staticRect)){
+            console.log("kolizja");
+            staticObject.remove();
+            staticObjectNodes = document.querySelectorAll(".present");
+            
+        }
+    });
     requestAnimationFrame(updatePosition);
 }
 
